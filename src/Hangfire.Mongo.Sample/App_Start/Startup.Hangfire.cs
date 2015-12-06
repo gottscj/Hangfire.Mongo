36455451e58cd1c79c80a21cfcdd4f5707b5b3fc
Sergey Zwezdin <sergey@zwezdin.com>
2015-12-06 18:29:04 +0500

    Build fix

--- src/Hangfire.Mongo.Sample/App_Start/Startup.Hangfire.cs
+++ src/Hangfire.Mongo.Sample/App_Start/Startup.Hangfire.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
-using Owin;
-using Hangfire.Mongo.Sample.Models;
+﻿using Owin;
 
 namespace Hangfire.Mongo.Sample
 {
