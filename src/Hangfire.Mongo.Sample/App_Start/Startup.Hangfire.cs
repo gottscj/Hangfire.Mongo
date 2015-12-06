be18f07c997aa1214e652e3c0da76da627981e5f
Sergey Zwezdin <sergey@zwezdin.com>
2015-12-06 10:03:38 +0500

    Sample project added

---
+++ src/Hangfire.Mongo.Sample/App_Start/Startup.Hangfire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using Hangfire.Mongo.Sample.Models;
+
+namespace Hangfire.Mongo.Sample
+{
+    public partial class Startup
+    {
+        public void ConfigureHangfire(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseMongoStorage("mongodb://localhost", "hangfire-mongo-sample");
+
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+        }
+    }
+}
---
+++ src/Hangfire.Mongo.Sample/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hangfire.Mongo.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FireAndForget(int id)
+        {
+            for (int i = 0; i < id; i++)
+            {
+                BackgroundJob.Enqueue(() => Debug.WriteLine("Hangfire task started."));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delayed()
+        {
+            BackgroundJob.Schedule(() => Console.WriteLine("Delayed Hangfire task started!"),
+                                   TimeSpan.FromMinutes(1));
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Recurring()
+        {
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Recurring Hangfire task started!"),
+                                     Cron.Minutely);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
