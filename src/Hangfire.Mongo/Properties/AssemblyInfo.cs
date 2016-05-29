642b2533b7f71a22772417fa3342663808d5f347
Sergey Zwezdin <sergey@zwezdin.com>
2016-05-29 07:03:43 +0500

    Version update

--- src/Hangfire.Mongo/Properties/AssemblyInfo.cs
+++ src/Hangfire.Mongo/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.5")]
-[assembly: AssemblyInformationalVersion("0.2.5")]
-[assembly: AssemblyFileVersion("0.2.5")]
+[assembly: AssemblyVersion("0.2.6")]
+[assembly: AssemblyInformationalVersion("0.2.6")]
+[assembly: AssemblyFileVersion("0.2.6")]
 [assembly: InternalsVisibleTo("Hangfire.Mongo.Tests")]
