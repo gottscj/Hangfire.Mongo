4870d4bed07057a67f55492096b2be58d60af8bb
Martin Lobger <martin@lobger.com>
2016-09-12 10:11:40 +0200

    Update build system to target MSBuild v14

--- build/build.cmd
+++ build/build.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd "%~dp0"
 .nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages
-packages\psake.4.4.2\tools\psake.cmd .\build.ps1 %*
+packages\psake.4.6.0\tools\psake.cmd .\build.ps1 %*
