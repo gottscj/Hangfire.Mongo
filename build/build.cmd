77be656836577242f7c1fbcc862be00353f5a944
Jonas Gottschau <Gottschau_Jonas@shure.dk>
2017-04-11 15:31:58 +0200

    Adding updates to AssemblyInfo.cs

--- build/build.cmd
+++ build/build.cmd
@@ -1,32 +1,42 @@
 @echo off
 
+set community_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
+set proofessional_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe
+set enterprise_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe
+
 set msbuild=""
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
-	set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+if exist "%community_msbuild%" (
+	set msbuild="%community_msbuild%"
+) else if exist "%proofessional_msbuild%" (
+	set msbuild="%proofessional_msbuild%"
+) else if exist "%enterprise_msbuild%" (
+	set msbuild="%enterprise_msbuild%"
 )
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
-	set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
-)
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
-	set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
-)
-echo %msbuild%
-if %msbuild% == "" (
-	echo no MSBuild found.
+
+set artifacts_sources_path="%cd%\artifacts\sources"
+set artifacts_bin_path="%cd%\artifacts\bin"
+set artifacts_nuget_path="%cd%\artifacts\nuget"
+set sources_path="%cd%\src"
+set build_output="%artifacts_sources_path%\Hangfire.Mongo\bin\Release"
+set build_number=%APPVEYOR_BUILD_NUMBER%
+set build_tag=%APPVEYOR_REPO_TAG%
+
+if %build_number%=="" (
+	echo AppVeyor APPVEYOR_BUILD_NUMBER vas not found!
+	set build_number=0.3.2.227
 	exit /B 1
 )
 
-set base_path=%cd%
-set version_file_path = \Hangfire.Mongo\Properties\AssemblyInfo.cs
-set artifacts_sources_path="%base_path%\artifacts\sources"
-set artifacts_bin_path="%base_path%\artifacts\bin"
-set artifacts_nuget_path="%base_path%\artifacts\nuget"
-set sources_path="%base_path%\src"
-set build_output="%artifacts_sources_path%\Hangfire.Mongo\bin\Release"
-rem set buildNumber=%APPVEYOR_BUILD_NUMBER%;
-rem set buildTag=%APPVEYOR_REPO_TAG%;
-set buildNumber=0.3.2.227
-set buildTag=0.3.2
+if %build_tag%=="" (
+	echo AppVeyor APPVEYOR_REPO_TAG vas not found!
+	set build_tag=0.3.2
+	exit /B 1
+)
+
+if %msbuild% == "" (
+	echo No MSBuild found.
+	exit /B 1
+)
 
 rem BUILD
 echo delete and create artifacts folders
@@ -37,17 +47,56 @@ mkdir %artifacts_nuget_path%
 mkdir %artifacts_sources_path%
 
 echo copying source files to artifacts folder
-xcopy /e /v /q %sources_path% %artifacts_sources_path%
+xcopy /e /v /q /f %sources_path% %artifacts_sources_path%
 
-rem TODO, update AssemblyInfo.cs
-
+echo Updating Version information
+call :UpdateAssemblyInfo
+if %errorlevel%==1 (
+	exit /B %errorlevel%
+)
 echo build project using selected MSBuild
 %msbuild% %artifacts_sources_path%\Hangfire.Mongo\Hangfire.Mongo.csproj /t:Rebuild /t:pack /p:Configuration="Release"
 
 echo copying build output to bin artifacts folder
-xcopy /e /v /q %build_output% %artifacts_bin_path%
+xcopy /e /v /q /f %build_output% %artifacts_bin_path%
 
 echo moving nuget to own folder in artifacts
-move /y %artifacts_bin_path%\Hangfire.Mongo.%buildTag%.nupkg %artifacts_nuget_path%
+move /y %artifacts_bin_path%\*.nupkg %artifacts_nuget_path%
 
+exit /B 0
+
+: UpdateAssemblyInfo
+set file=%cd%\artifacts\sources\Hangfire.Mongo\Properties\AssemblyInfo.cs
+
+if not exist "%file%" (
+    echo Could not find "%file%"
+    exit /B 1
+)
+
+if exist "%file%.new" (
+    del "%file%.new"
+)
+
+for /f "tokens=*" %%a in (%file%) do (
+	setlocal enabledelayedexpansion
+    set line=%%a
+    set assembly_version=!line:AssemblyVersion=!
+    set assembly_file_version=!line:AssemblyFileVersion=!
+    set assembly_informational_version=!line:AssemblyInformationalVersion=!
+    set startChars=!line:~0,2!
+
+    if not !startChars!==// (
+        if not !assembly_version!==%%a (
+            set "line=[assembly: AssemblyVersion("%build_number%")]"
+        ) else if not !assembly_file_version!==%%a (
+            set "line=[assembly: AssemblyFileVersion("%build_number%")]"
+        ) else if not !assembly_informational_version!==%%a (
+            set "line=[assembly: AssemblyInformationalVersion("%build_tag%")]"
+        )
+    ) 
+    echo !line! >> "%file%.new"
+	endlocal
+)
+
+move "%file%.new" "%file%"
 exit /B 0
