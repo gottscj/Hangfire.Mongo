883b2325d0be2efb7defc42e702c1f65d7be25d8
Jonas Gottschau <jonas@gottschau.dk>
2017-04-24 22:15:15 +0200

    Upgrade projects and scripts to vs2017 (#63)
    * Upgrading projects to new csproj structure for NETCore and netstandard
    
    * Updating Nuget.exe, next fix psake to use MsBuild v15
    
    * Simplifying build script, utilizing MSBuild is now integrated with nuget
    
    * Adding updates to AssemblyInfo.cs
    
    * Print build number and build tag on build
    
    * Simplifying build script

--- build/build.cmd
+++ build/build.cmd
@@ -1,4 +1,51 @@
 @echo off
-cd "%~dp0"
-.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages
-packages\psake.4.6.0\tools\psake.cmd .\build.ps1 %*
+
+set community_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
+set proofessional_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe
+set enterprise_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe
+
+set msbuild=""
+if exist "%community_msbuild%" (
+	set msbuild="%community_msbuild%"
+) else if exist "%proofessional_msbuild%" (
+	set msbuild="%proofessional_msbuild%"
+) else if exist "%enterprise_msbuild%" (
+	set msbuild="%enterprise_msbuild%"
+)
+
+set artifacts_sources_path="%cd%\artifacts\sources"
+set artifacts_bin_path="%cd%\artifacts\bin"
+set artifacts_nuget_path="%cd%\artifacts\nuget"
+set sources_path="%cd%\src"
+set build_output="%artifacts_sources_path%\Hangfire.Mongo\bin\Release"
+
+echo build number : %APPVEYOR_BUILD_NUMBER%
+echo build tag    : %APPVEYOR_REPO_TAG%
+echo build version: %APPVEYOR_BUILD_VERSION%
+
+if %msbuild% == "" (
+	echo No MSBuild found.
+	exit /B 1
+)
+
+rem BUILD
+echo delete and create artifacts folders
+rmdir artifacts /s /q
+
+mkdir %artifacts_bin_path%
+mkdir %artifacts_nuget_path%
+mkdir %artifacts_sources_path%
+
+echo copying source files to artifacts folder
+xcopy /e /v /q /f %sources_path% %artifacts_sources_path%
+
+echo build project using selected MSBuild
+%msbuild% %artifacts_sources_path%\Hangfire.Mongo\Hangfire.Mongo.csproj /t:Rebuild /t:pack /p:Configuration="Release"
+
+echo copying build output to bin artifacts folder
+xcopy /e /v /q /f %build_output% %artifacts_bin_path%
+
+echo moving nuget to own folder in artifacts
+move /y %artifacts_bin_path%\*.nupkg %artifacts_nuget_path%
+
+exit /B 0
--- src/Hangfire.Mongo/Properties/AssemblyInfo.cs
+++ src/Hangfire.Mongo/Properties/AssemblyInfo.cs
@@ -1,19 +1,17 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Hangfire.Mongo")]
-[assembly: AssemblyDescription("Hangfire MongoDB Storage. https://github.com/sergun/Hangfire.Mongo")]
+[assembly: AssemblyDescription("MongoDB storage implementation for Hangfire (background job system for ASP.NET applications).")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hangfire.Mongo")]
 [assembly: AssemblyCopyright("Copyright © 2014-2016 Sergey Zwezdin, Martin Lobger, Jonas Gottschau")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -21,7 +19,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("02c80b18-125e-473f-be8b-f50dcd86396b")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
