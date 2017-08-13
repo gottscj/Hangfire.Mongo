7cb26409ea3f8cdb9a8a2699c0221ee59de7f766
Jonas Gottschau <jonas@gottschau.dk>
2017-08-13 20:25:33 +0200

    Added project

---
+++ src/Hangfire.Mongo.Sample.ASPNetCore/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hangfire.Mongo.Sample.ASPNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FireAndForget(int id)
+        {
+            for (int i = 0; i < id; i++)
+            {
+                BackgroundJob.Enqueue(() => PrintToConsole("Hangfire fire-and-forget task started."));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delayed(int id)
+        {
+            for (int i = 0; i < id; i++)
+            {
+                BackgroundJob.Schedule(() => PrintToConsole("Hangfire delayed task started!"), TimeSpan.FromMinutes(1));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Recurring()
+        {
+            RecurringJob.AddOrUpdate(() => PrintToConsole("Hangfire recurring task started!"), Cron.Minutely);
+
+            return RedirectToAction("Index");
+        }
+        public static void PrintToConsole(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
