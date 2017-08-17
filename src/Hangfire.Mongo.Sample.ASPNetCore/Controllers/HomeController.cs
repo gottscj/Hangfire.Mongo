596c3954408f8c065670501d34eb81b7872c5bbe
Jonas Gottschau <jonas@gottschau.dk>
2017-08-17 13:39:45 +0200

    Add asp net core sample (#91)
    * Small fixes for building out of the box on linux
    Remove MongoDistributedLock call in MongoWriteOnlyTransaction
    
    * Added project
    
    * Add AspNetCore sample
    
    * Add AspNetCore sample.
    Move tasks.json and launch.json to root folder
    
    * Remove reference to InteropServices
    
    * Wrapping up new ASPNetCore sample

---
+++ src/Hangfire.Mongo.Sample.ASPNetCore/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hangfire.Mongo.Sample.ASPNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FireAndForget(int id)
+        {
+            for (int i = 0; i < id; i++)
+            {
+                var index = i;
+                BackgroundJob.Enqueue(() => PrintToDebug($@"Hangfire fire-and-forget task started ({index}) - {Guid.NewGuid()}"));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delayed(int id)
+        {
+            for (int i = 0; i < id; i++)
+            {
+                var index = i;
+                BackgroundJob.Schedule(() => PrintToDebug($@"Hangfire delayed task started ({index}) - {Guid.NewGuid()}"), TimeSpan.FromMinutes(1));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Recurring()
+        {
+            RecurringJob.AddOrUpdate(() => PrintToDebug($@"Hangfire recurring task started - {Guid.NewGuid()}"), Cron.Minutely);
+
+            return RedirectToAction("Index");
+        }
+
+        public static void PrintToDebug(string message)
+        {
+            Debug.WriteLine(message);
+        }
+    }
+}
---
+++ src/Hangfire.Mongo.Sample.ASPNetCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Hangfire.Mongo.Sample.ASPNetCore
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
