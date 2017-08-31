e6b4bec5585de56bf11f7387dddca51899753820
Martin Lobger <martin@lobger.com>
2017-08-31 15:57:13 +0200

    Force ignoring globally selected bson naming convention

--- src/Hangfire.Mongo/Dto/JobDto.cs
+++ src/Hangfire.Mongo/Dto/JobDto.cs
@@ -10,19 +10,26 @@ namespace Hangfire.Mongo.Dto
     {
         [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
         public string Id { get; set; }
-        
+
+        [BsonElement(nameof(StateName))]
         public string StateName { get; set; }
 
+        [BsonElement(nameof(InvocationData))]
         public string InvocationData { get; set; }
 
+        [BsonElement(nameof(Arguments))]
         public string Arguments { get; set; }
 
+        [BsonElement(nameof(Parameters))]
         public Dictionary<string, string> Parameters { get; set; } = new Dictionary<string, string>();
 
+        [BsonElement(nameof(StateHistory))]
         public StateDto[] StateHistory { get; set; } = new StateDto[0];
 
+        [BsonElement(nameof(CreatedAt))]
         public DateTime CreatedAt { get; set; }
 
+        [BsonElement(nameof(ExpireAt))]
         public DateTime? ExpireAt { get; set; }
     } 
 #pragma warning restore 1591
--- src/Hangfire.Mongo/Dto/ServerDto.cs
+++ src/Hangfire.Mongo/Dto/ServerDto.cs
@@ -9,8 +9,10 @@ namespace Hangfire.Mongo.Dto
         [BsonId]
         public string Id { get; set; }
 
+        [BsonElement(nameof(Data))]
         public string Data { get; set; }
 
+        [BsonElement(nameof(LastHeartbeat))]
         public DateTime? LastHeartbeat { get; set; }
     }
 #pragma warning restore 1591
