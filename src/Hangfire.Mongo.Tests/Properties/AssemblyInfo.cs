5df6d87587a4d25edf01715c69e20f2e819e2499
Martin Lobger <martin@lobger.com>
2017-10-13 10:18:45 +0200

    Release 0.5.6

--- src/Hangfire.Mongo.Tests/Properties/AssemblyInfo.cs
+++ src/Hangfire.Mongo.Tests/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using Xunit;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.5")]
-[assembly: AssemblyFileVersion("0.5.5")]
-[assembly: AssemblyInformationalVersion("0.5.5")]
+[assembly: AssemblyVersion("0.5.6")]
+[assembly: AssemblyFileVersion("0.5.6")]
+[assembly: AssemblyInformationalVersion("0.5.6")]
