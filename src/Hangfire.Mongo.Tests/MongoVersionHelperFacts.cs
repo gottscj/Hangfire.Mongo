35a6b81951aefb998cef6227ccf88dd4234b10d8
Jonas Gottschau <jonas@gottschau.dk>
2019-10-21 21:13:04 +0200

    Create 0.6.5 branch (#218)
    make getting mongdb version more resilient

---
+++ src/Hangfire.Mongo.Tests/MongoVersionHelperFacts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Hangfire.Common;
+using Hangfire.Mongo.Tests.Utils;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Moq;
+using Xunit;
+
+namespace Hangfire.Mongo.Tests
+{
+#pragma warning disable 1591
+    [Collection("Database")]
+    public class MongoVersionHelperFacts
+    {
+        [Fact]
+        public void GetVersion_HasAdditionalInfo_Success()
+        {
+            // ARRANGE
+            var dbMock = new Mock<IMongoDatabase>(MockBehavior.Strict);
+            var command = new JsonCommand<BsonDocument>("{'buildinfo': 1}");
+            dbMock.Setup(m => m.RunCommand(It.Is<JsonCommand<BsonDocument>>(b => b.Json.Contains("buildinfo")), null, CancellationToken.None))
+                .Returns(new BsonDocument
+            {
+                ["version"] = "3.6.4-1.2"
+            });
+            
+            // ACT
+            var version = MongoVersionHelper.GetVersion(dbMock.Object);
+            
+            // ASSERT
+            Assert.Equal(version, new Version(3, 6, 4));
+        }
+
+        [Fact]
+        public void GetVersion_FromDb_Success()
+        {
+            // ARRANGE
+            using (var connection = ConnectionUtils.CreateDbContext())
+            {
+                // ACT
+                var version = MongoVersionHelper.GetVersion(connection.Database);
+            }
+
+            // ASSERT
+            // no exception
+        }
+    }
+#pragma warning restore 1591
+}
---
+++ src/Hangfire.Mongo/MongoVersionHelper.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Hangfire.Mongo
+{
+    internal static class MongoVersionHelper
+    {
+        public static Version GetVersion(IMongoDatabase database)
+        {
+            var command = new JsonCommand<BsonDocument>("{'buildinfo': 1}");
+            var serverStatus = database.RunCommand(command);
+            if (!serverStatus.Contains("version"))
+            {
+                throw new InvalidOperationException($"Could not get 'buildinfo' from database: got: " +
+                                                    serverStatus.ToJson());
+            }
+            
+            var versionSplit = serverStatus["version"].AsString.Split('.');
+            var major = int.Parse(versionSplit[0]);
+            var minor = int.Parse(versionSplit[1]);
+            var build = int.Parse(new string(versionSplit[2].TakeWhile(Char.IsDigit).ToArray()));
+
+            return new Version(major, minor, build);
+
+        }
+    }
+}
