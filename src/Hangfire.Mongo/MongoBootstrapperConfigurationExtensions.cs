37d2ee96ef3f3035a48cf45dcfd16576718a2d0e
Cédric Luthi <cedric.luthi@gmail.com>
2019-11-15 10:18:44 +0100

    Add support for specifying the database name in the connection string

--- src/Hangfire.Mongo/MongoBootstrapperConfigurationExtensions.cs
+++ src/Hangfire.Mongo/MongoBootstrapperConfigurationExtensions.cs
@@ -53,9 +53,23 @@ namespace Hangfire.Mongo
         /// Configure Hangfire to use MongoDB storage
         /// </summary>
         /// <param name="configuration">Configuration</param>
+        /// <param name="connectionString">Connection string for Mongo database, for example 'mongodb://username:password@host:port/database'</param>
+        /// <remarks>The connection string must include the database name</remarks>
+        /// <returns></returns>
+        public static MongoStorage UseMongoStorage(this IGlobalConfiguration configuration,
+            string connectionString)
+        {
+            return UseMongoStorage(configuration, connectionString, new MongoStorageOptions());
+        }
+
+        /// <summary>
+        /// Configure Hangfire to use MongoDB storage
+        /// </summary>
+        /// <param name="configuration">Configuration</param>
         /// <param name="connectionString">Connection string for Mongo database, for example 'mongodb://username:password@host:port'</param>
         /// <param name="databaseName">Name of database at Mongo server</param>
         /// <returns></returns>
+        [Obsolete("Please use `UseMongoStorage(this IGlobalConfiguration configuration, string connectionString)` instead, providing the database name in the connection string.")]
         public static MongoStorage UseMongoStorage(this IGlobalConfiguration configuration,
             string connectionString,
             string databaseName)
@@ -67,10 +81,33 @@ namespace Hangfire.Mongo
         /// Configure Hangfire to use MongoDB storage
         /// </summary>
         /// <param name="configuration">Configuration</param>
+        /// <param name="connectionString">Connection string for Mongo database, for example 'mongodb://username:password@host:port/database'</param>
+        /// <param name="storageOptions">Storage options</param>
+        /// <exception cref="ArgumentException">Thrown if the connection string does not include the database name</exception>
+        /// <returns></returns>
+        public static MongoStorage UseMongoStorage(this IGlobalConfiguration configuration,
+            string connectionString,
+            MongoStorageOptions storageOptions)
+        {
+            var mongoUrlBuilder = new MongoUrlBuilder(connectionString);
+            var databaseName = mongoUrlBuilder.DatabaseName;
+            if (string.IsNullOrEmpty(databaseName))
+            {
+                throw new ArgumentException("The connection string must include the database name, see https://docs.mongodb.com/manual/reference/connection-string/", nameof(connectionString));
+            }
+            var mongoClientSettings = MongoClientSettings.FromConnectionString(connectionString);
+            return UseMongoStorage(configuration, mongoClientSettings, databaseName, storageOptions);
+        }
+
+        /// <summary>
+        /// Configure Hangfire to use MongoDB storage
+        /// </summary>
+        /// <param name="configuration">Configuration</param>
         /// <param name="connectionString">Connection string for Mongo database, for example 'mongodb://username:password@host:port'</param>
         /// <param name="databaseName">Name of database at Mongo server</param>
         /// <param name="storageOptions">Storage options</param>
         /// <returns></returns>
+        [Obsolete("Please use `UseMongoStorage(this IGlobalConfiguration configuration, string connectionString, MongoStorageOptions storageOptions)` instead, providing the database name in the connection string.")]
         public static MongoStorage UseMongoStorage(this IGlobalConfiguration configuration,
             string connectionString,
             string databaseName,
@@ -113,7 +150,7 @@ namespace Hangfire.Mongo
 
             return storage;
         }
-        
+
         /// <summary>
         /// Configure Hangfire to use MongoDB storage
         /// </summary>
