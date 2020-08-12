51fd76fcb98f96dd10cf929b1b5db1bc6849c664
bulbulhas <11867439+bulbulhas@users.noreply.github.com>
2020-08-12 11:54:24 +0300

    Index Update (#247)
    Index defined for score field in jobGraph collection

---
+++ src/Hangfire.Mongo/Migration/Steps/Version17/01_UpdateIndexes.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Hangfire.Mongo.Migration.Steps.Version17
+{
+    internal class UpdateIndexes : IndexMigration, IMongoMigrationStep
+    {
+        public MongoSchema TargetSchema => MongoSchema.Version17;
+
+        public long Sequence => 1;
+
+        public bool Execute(IMongoDatabase database, MongoStorageOptions storageOptions, IMongoMigrationContext migrationContext)
+        {
+            var jobGraph = database.GetCollection<BsonDocument>($@"{storageOptions.Prefix}.jobGraph");
+            var indexBuilder = Builders<BsonDocument>.IndexKeys;
+
+            TryCreateIndexes(jobGraph, indexBuilder.Ascending, "Score");
+
+            return true;
+        }
+
+    }
+
+}
