4281da5c167e2d3669fe9fd2f94e2d83d3148d48
Jonas Gottschau <jlg@brandarmies.com>
2020-11-08 21:58:47 +0100

    update build.cmd

--- build/build.cmd
+++ build/build.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-set community_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe
-set proofessional_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe
-set enterprise_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe
+set community_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe
+set proofessional_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe
+set enterprise_msbuild=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe
 
 set msbuild=""
 if exist "%community_msbuild%" (
