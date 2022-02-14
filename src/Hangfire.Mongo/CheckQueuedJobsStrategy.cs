c88561fbd32603e1a2da83c0f0e8ab6f998a5d63
Corentin Altepe <corentin.altepe@gmail.com>
2022-02-14 09:00:32 +0100

    Documented usage of CheckQueuedJobsStrategy in Readme. (#310)
    * Documented usage of CheckQueuedJobsStrategy in Readme.
    
    This addresses concerns discussed in issue #300.
    
    Other minor rewording of a note in the readme and comments of the  CheckQueuedJobsStrategy class.
    
    * Fixed word in readme.

--- src/Hangfire.Mongo/CheckQueuedJobsStrategy.cs
+++ src/Hangfire.Mongo/CheckQueuedJobsStrategy.cs
@@ -9,18 +9,18 @@ namespace Hangfire.Mongo
     {
         /// <summary>
         /// Will poll periodically using 'QueuePollInterval',
-        /// recommended for large installments
+        /// recommended for large installments.
         /// </summary>
         Poll = 0,
         /// <summary>
-        /// Use change streams to watch for enqueued jobs. default setting,
-        /// will still poll using 'QueuePollInterval'
+        /// Use change streams to watch for enqueued jobs. Default setting.
+        /// Will still poll using 'QueuePollInterval'.
         /// </summary>
         Watch = 1,
         /// <summary>
         /// Use a capped, tailable collection to notify nodes of enqueued jobs.
-        /// will still poll using 'QueuePollInterval'
-        /// Works with single node/ test setups
+        /// Will still poll using 'QueuePollInterval'.
+        /// Works with single node and test setups.
         /// </summary>
         TailNotificationsCollection = 2
     }
