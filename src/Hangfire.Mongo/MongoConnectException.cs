94fa9bfeb3e205500f406db2fd6b3f5412adde9c
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2022-06-18 16:59:34 +0200

    Fix hardcoded timeout in MongoConnectException message.

--- src/Hangfire.Mongo/MongoConnectException.cs
+++ src/Hangfire.Mongo/MongoConnectException.cs
@@ -13,9 +13,10 @@ namespace Hangfire.Mongo
         /// </summary>
         /// <param name="dbContext"></param>
         /// <param name="connectionString"></param>
+        /// <param name="connectionCheckTimeout"></param>
         /// <param name="e"></param>
-        public MongoConnectException(HangfireDbContext dbContext, string connectionString, Exception e) 
-            : base($"\r\nDid not receive ping response from '{dbContext.Database.DatabaseNamespace.DatabaseName}' within 1000ms\r\n" +
+        public MongoConnectException(HangfireDbContext dbContext, string connectionString, TimeSpan connectionCheckTimeout, Exception e) 
+            : base($"\r\nDid not receive ping response from '{dbContext.Database.DatabaseNamespace.DatabaseName}' within {connectionCheckTimeout.TotalMilliseconds}ms\r\n" +
                    $"assuming not able to connect to '{connectionString}'\r\n" +
                    $"you can disable database ping via the MongoStorageOption 'CheckConnection' field\r\n", e)
         {
