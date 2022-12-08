e376ec96dbb462d1d5cf3788a2ff9137316e3064
Jonas Gottschau <jonas@gottschau.dk>
2022-12-08 22:26:47 +0100

    use service for tests instead of mongo2go

--- src/Hangfire.Mongo.Tests/Utils/ConnectionUtils.cs
+++ src/Hangfire.Mongo.Tests/Utils/ConnectionUtils.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.IO;
-using Hangfire.Mongo.Database;
+﻿using Hangfire.Mongo.Database;
 using Hangfire.Mongo.Migration.Strategies;
 using Hangfire.Mongo.Migration.Strategies.Backup;
-using Microsoft.Extensions.Logging.Abstractions;
-using Mongo2Go;
 using MongoDB.Driver;
-using Xunit.Abstractions;
-using Xunit.Sdk;
 
 [assembly: Xunit.TestFramework("Hangfire.Mongo.Tests.Utils.ConnectionUtils", "Hangfire.Mongo.Tests")]
 
 namespace Hangfire.Mongo.Tests.Utils
 {
 #pragma warning disable 1591
-    public class ConnectionUtils : XunitTestFramework
+    public class ConnectionUtils
     {
-        private static Mongo2Go.MongoDbRunner _runner;
         private const string DefaultDatabaseName = @"Hangfire-Mongo-Tests";
-
+        private const string ConnectionString = "mongodb://localhost:27017";
         public static MongoStorage CreateStorage(string databaseName = null)
         {
             var storageOptions = new MongoStorageOptions
@@ -35,33 +28,19 @@ namespace Hangfire.Mongo.Tests.Utils
         
         public static MongoStorage CreateStorage(MongoStorageOptions storageOptions, string databaseName=null)
         {
-            var mongoClientSettings = MongoClientSettings.FromConnectionString(_runner.ConnectionString);
+            var mongoClientSettings = MongoClientSettings.FromConnectionString(ConnectionString);
             return new MongoStorage(mongoClientSettings, databaseName ?? DefaultDatabaseName, storageOptions);
         }
 
         public static HangfireDbContext CreateDbContext(string dbName = null)
         {
-            return new HangfireDbContext(_runner.ConnectionString, dbName ?? DefaultDatabaseName);
+            return new HangfireDbContext(ConnectionString, dbName ?? DefaultDatabaseName);
         }
 
         public static void DropDatabase()
         {
-            var client = new MongoClient(_runner.ConnectionString);
+            var client = new MongoClient(ConnectionString);
             client.DropDatabase(DefaultDatabaseName);
-        }
-        
-
-        public ConnectionUtils(IMessageSink messageSink) : base(messageSink)
-        {
-            var homePath = (Environment.OSVersion.Platform == PlatformID.Unix || 
-                            Environment.OSVersion.Platform == PlatformID.MacOSX)
-                ? Environment.GetEnvironmentVariable("HOME")
-                : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
-            _runner = MongoDbRunner.Start(
-                dataDirectory: Path.Combine(homePath, "db"),
-                singleNodeReplSet: true,
-                logger: new NullLogger<MongoDbRunner>());
-            DisposalTracker.Add(_runner);
         }
     }
 #pragma warning restore 1591
